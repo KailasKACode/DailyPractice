aa0905b2a8d249c296e60031e56b89d963697f42
ResonaHub <Andhalkark@gmail.com>
2019-09-17 17:30:19 +0530

    MainMethodDataCleared

--- src/FindTwoMaxNumber.java
+++ src/FindTwoMaxNumber.java
@@ -1,22 +1,7 @@
 
 public class FindTwoMaxNumber {
 	public static void main(String[] args) {
-		int a[] = { 99, 6, 3, 8, 9, 2, 15, 58, 59, 95 };
-		int firstlarge = 0;
-		int secondlarge = 1;
-		for (int i = 0; i < a.length; i++) {
-			if (firstlarge < a[i]) {
-				secondlarge = firstlarge;
-				firstlarge = a[i];
-
-			} else if (secondlarge < a[i]) {
-				secondlarge = a[i];
-			}
-		}
-		int product = firstlarge * secondlarge;
-
-		System.out.println(firstlarge + "  " + secondlarge + "  Largest two products are: " + product);
-		int b[] = { 4, 9, 6, 7, 2, 3, 8 };
+		int b[] = { 4, 9, 6, 7, 2, 3, 8, 96 };
 		productMbx(b);
 	}
 
