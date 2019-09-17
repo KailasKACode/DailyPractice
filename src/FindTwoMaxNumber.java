85d9603870bbbb62cfdaf3dd31ee262dbc661c64
ResonaHub <Andhalkark@gmail.com>
2019-09-17 09:55:45 +0530

    SomeChangesMade

--- src/FindTwoMaxNumber.java
+++ src/FindTwoMaxNumber.java
@@ -1,16 +1,23 @@
 
 public class FindTwoMaxNumber {
 	public static void main(String[] args) {
-		int a[] = { 1, 6, 3, 8, 9, 2, 15, 58 };
+		int a[] = { 99, 6, 3, 8, 9, 2, 15, 58, 8, 59, 95, 105 };
 		int temp = 0;
-		int i = a.length - 1;
-		for (; i >= 0; i--) {
-			System.out.print(" " + a[i]+"  ");
+		int i = 0;
+		int j = 1;
+		for (; i < a.length; i++) {
+			for (; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+
+				}
+
+				System.out.println(a[j]);
+			}
+
 		}
-		
-		
-
 	}
-
 
 }
